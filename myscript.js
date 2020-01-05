d6558d1eda924ea11a62c0691c5f97cde63cc84e
Edmund Quashie <dwolino@gmail.com>
2020-01-05 10:13:21 +0000

    new commit

---
+++ myscript.js
@@ -0,0 +1,13 @@
+var a = 2+3
+var b = 24*67
+var subtraction = 1200-3400
+var division =123/34
+var complex = 23+12-100/2*123
+``
+console.log(complex)
+console.log(a)
+console.log(division)
+console.log(subtraction)
+
+var age = 20
+if (age>21) {document.write ("unqualified");}else{document.write("input the correct age");}
